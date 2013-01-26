47bbd437440111b2c8b35c414e9c591229b79dcb
Amos Chan <amos.chan@chapps.org>
2013-01-26 01:47:18 -0500

    front page styling + mustache

--- js/bbui-reddit.js
+++ js/bbui-reddit.js
@@ -1,20 +1,7 @@
 function bbifyPost(post, callback) {
+  var linkTemplate = $('#linkTemplate').html();
+  var html = Mustache.to_html(linkTemplate, post.data);
   var div = $('<div/>');
-  var postTitleContainer = $('<div/>').addClass('container').addClass('post');
-  var row = $('<div/>').addClass('row');
-  row.appendTo(postTitleContainer);
-  var thread = $('<div/>', { text: post.data.title }).addClass('span12');
-  thread.appendTo(row);
-  postTitleContainer.appendTo(div);
-
-  var detailsContainer = $('<div/>').addClass('container').addClass('details');
-  var details = $('<div/>').addClass('row');
-  var detailsText = post.data.subreddit + " • " + 
-                    post.data.score + " • " + 
-                    post.data.domain + " • " + 
-                    post.data.author;
-  $('<div/>', { text: detailsText }).addClass('span12').appendTo(details);
-  details.appendTo(detailsContainer);
-  detailsContainer.appendTo(div);
+  div.html(html);
   callback(div);
 }
