9e1a6122635d8ecf044f95056053999378e5364c
Amos Chan <amos.chan@chapps.org>
2013-01-26 02:02:57 -0500

    NumComments. Checkpoint before trying out Bootstrap 3.

--- js/bbui-reddit.js
+++ js/bbui-reddit.js
@@ -1,6 +1,12 @@
-function bbifyPost(post, callback) {
+function bbifyPost(link, callback) {
   var linkTemplate = $('#linkTemplate').html();
-  var html = Mustache.to_html(linkTemplate, post.data);
+  var html = Mustache.to_html(linkTemplate, 
+                              { title: link.data.title,
+                                numComments: link.data.num_comments,
+                                subreddit: link.data.subreddit,
+                                score: link.data.score,
+                                domain: link.data.domain,
+                                author: link.data.author });
   var div = $('<div/>');
   div.html(html);
   callback(div);
