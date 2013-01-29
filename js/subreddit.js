9c68bb2756efd4d8dbb61c06650a3572137d768e
Amos Chan <amos.chan@chapps.org>
2013-01-28 20:05:31 -0500

    Move subreddit loading to ondomready

--- js/subreddit.js
+++ js/subreddit.js
@@ -5,3 +5,23 @@ function subredditScreenReady(element, params) {
     });
   });
 }
+
+function subredditDomReady(element, params) {
+  alert('here we go again');
+  var subredditDefined = (typeof params !== "undefined" && typeof params.subreddit !== "undefined");
+  var subreddit =  subredditDefined ? params.subreddit : false;
+
+  var visited = element.getAttribute('data-snooby-visited');
+  console.log('visited? ' + JSON.stringify(visited));
+  if (typeof visited === 'undefined' || visited !== true) {
+    element.setAttribute('data-snooby-visited', true);
+    console.log('marked element as visited');
+    listing(subreddit, function(post) {
+      $('#loading').hide();
+      $('#listing').show();
+      bbifyPost(post, function(bbPost) {
+        $(bbPost).appendTo('#listing');
+      });
+    });
+  }
+}
