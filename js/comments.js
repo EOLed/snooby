bf9521f35e47868879ef19d26e700de061e44136
Amos Chan <amos.chan@chapps.org>
2013-02-19 16:47:33 -0500

    Screen transitions. #17

--- js/comments.js
+++ js/comments.js
@@ -1,29 +1,25 @@
 var _comments = {
   onScreenReady: function(element, params) {
-  },
-
-  onDomReady: function(element, params) {
     var domain = params.link.data.domain;
     var selfPost = domain === 'self.' + params.link.data.subreddit;
-    var headerTemplate = $(selfPost ? '#selfPostHeaderTemplate' : '#linkHeaderTemplate').html();
+    var headerTemplate = element.getElementById(selfPost ? 'selfPostHeaderTemplate' : 
+                                                           'linkHeaderTemplate').innerHTML;
     var html = Mustache.to_html(headerTemplate,
                                 { title: selfPost ? params.link.data.title : 
                                                     '<a href="' + params.link.data.url + '">' + params.link.data.title + '</a>',
                                   body: selfPost ? SnuOwnd.getParser().render(params.link.data.selftext) : null,
                                   domain: params.link.data.domain,
                                   numComments: params.link.data.num_comments });
-    var header = $('<div/>');
-    header.html(html);
+    element.getElementById('linkHeader').innerHTML = html;
 
-    html = Mustache.to_html($('#linkHeaderDetailsTemplate').html(),
+    html = Mustache.to_html(element.getElementById('linkHeaderDetailsTemplate').innerHTML,
                             { author: params.link.data.author,
                               score: params.link.data.score,
                               time: moment.unix(params.link.data.created_utc).fromNow() });
-    var details = $('<div/>');
-    details.html(html);
+    element.getElementById('linkDetails').innerHTML = html;
+  },
 
-    $('#linkHeader').append(header);
-    $('#linkDetails').append(details);
+  onDomReady: function(element, params) {
     $('#loading').show();
 
     app.comments(params.link.data.permalink, params.link.data.author, function(comment) {
