def1190378f70d9246ef0e3e74e3e25e236098a1
Amos Chan <amos.chan@chapps.org>
2013-02-19 13:14:29 -0500

    Snooby Gold moved to action overflow. Fixes #7.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Snooby
 
 Icons by http://icons.yootheme.com/ and fortawesome.github.com/Font-Awesome
 
+Myers Design Limited: http://myersdesign.com/resources/
+
--- js/settings.js
+++ js/settings.js
@@ -68,52 +68,5 @@ var _settings = {
       element.getElementById('currentLogin').innerHTML = html;
       element.getElementById('accountPanel').style.display = 'block';
     }
-  },
-
-  onDomReady: function(element, params) {
-    var onsuccess = function(data) {
-      if (data.subscriptionExists) {
-        document.getElementById('supportSnooby').style.display = 'none';
-        document.getElementById('snoobyGoldPurchased').style.display = 'block';
-      } else {
-        document.getElementById('supportSnooby').style.display = 'block';
-        document.getElementById('snoobyGoldPurchased').style.display = 'none';
-      }
-    };
-
-    var onfailure = function(error) {
-      document.getElementById('supportSnooby').style.display = 'block';
-      document.getElementById('snoobyGoldPurchased').style.display = 'none';
-    };
-
-    blackberry.payment.checkExisting({ sku: 'SNBY-002' }, onsuccess, onfailure);
-  },
-
-  purchaseSnoobyGold: function() {
-    document.getElementById('snoobyGoldButton')
-            .setCaption('<i class="icon-trophy"></i> I want Snooby Gold! <i class="icon-spinner icon-spin"></i>');
-    blackberry.payment.purchase({ digitalGoodSKU: 'SNBY-002',
-                                  purchaseAppName: 'Snooby',
-                                  metaData: 'v1.0.2 - Settings screen' }, 
-                                 this.onSnoobyGoldPurchase,
-                                 this.onSnoobyGoldPurchaseFailed);
-  },
-
-  onSnoobyGoldPurchase: function(data) {
-    document.getElementById('snoobyGoldButton').setCaption('<i class="icon-trophy"></i> I want Snooby Gold!');
-    document.getElementById('supportSnooby').style.display = 'none';
-    document.getElementById('snoobyGoldPurchased').style.display = 'block';
-  },
-
-  onSnoobyGoldPurchaseFailed: function(error) {
-    document.getElementById('snoobyGoldButton').setCaption('<i class="icon-trophy"></i> I want Snooby Gold!');
-
-    if (error.errorID !== 1) {
-        blackberry.ui.dialog.customAskAsync('An error occurred and Snooby Gold was not purchased.',
-                                            ['OK'],
-                                            null,
-                                            { title: 'Payment Error' });
-
-    }
   }
 };
