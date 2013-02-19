6fad456eb2d090c92bc7ff1f533a408cdd733a62
Amos Chan <amos.chan@chapps.org>
2013-02-19 02:51:32 -0500

    In-app purchase for Snooby Gold. #7

--- tests/spec/SettingsSpec.js
+++ tests/spec/SettingsSpec.js
@@ -1,4 +1,7 @@
-var blackberry = { ui: { dialog: { customAskAsync: function() {} } } };
+var blackberry = { ui: { dialog: { customAskAsync: function() {} } },
+                   payment: { developmentMode: false, 
+                              checkExisting: function(data,onsuccess, onfail) {}, 
+                              purchase: function(data, onsuccess, onfail) {} } };
 describe('Settings', function() {
   it('logs in with the username and password provided from form', function() {
     var getElementById = sinon.stub(document, 'getElementById');
@@ -75,6 +78,7 @@ describe('Settings', function() {
     getElementById.restore();
     subreddits.restore();
     login.restore();
+    blackberry.ui.dialog.customAskAsync.restore();
   });
 
   it('doesn\'t reload subreddits upon bad login', function() {
@@ -139,5 +143,105 @@ describe('Settings', function() {
     getElementById.restore();
     subreddits.restore();
     logout.restore();
+  });
+
+  it('No error dialog is displayed when Snooby Gold purchase is canceled by user', function() {
+    var getElementById = sinon.stub(document, 'getElementById');
+    getElementById.withArgs('snoobyGoldButton').returns({ setCaption: function(s) {} });
+
+    var errorDialog = sinon.spy(blackberry.ui.dialog, 'customAskAsync');
+
+    var purchase = sinon.stub(blackberry.payment, 'purchase', function(data, onsuccess, onfail) {
+      onfail({ errorID: 1 });
+    });
+
+    _settings.purchaseSnoobyGold();
+    expect(errorDialog.called).toBeFalsy();
+    blackberry.payment.purchase.restore();
+    document.getElementById.restore();
+    blackberry.ui.dialog.customAskAsync.restore();
+  });
+
+  it('An error dialog is displayed when failing to purchase Snooby Gold', function() {
+    var getElementById = sinon.stub(document, 'getElementById');
+    getElementById.withArgs('snoobyGoldButton').returns({ setCaption: function(s) {} });
+
+    var errorDialog = sinon.spy(blackberry.ui.dialog, 'customAskAsync');
+
+    var purchase = sinon.stub(blackberry.payment, 'purchase', function(data, onsuccess, onfail) {
+      onfail({ errorID: 12 });
+    });
+
+    _settings.purchaseSnoobyGold();
+    expect(errorDialog.called).toBeTruthy();
+    blackberry.payment.purchase.restore();
+    document.getElementById.restore();
+    blackberry.ui.dialog.customAskAsync.restore();
+  });
+
+  it('calls onSnoobyGoldFailed() when failing to purchase Snooby Gold', function() {
+    var getElementById = sinon.stub(document, 'getElementById');
+    getElementById.withArgs('snoobyGoldButton').returns({ setCaption: function(s) {} });
+
+    var snoobyPurchase = sinon.stub(_settings, 'onSnoobyGoldPurchaseFailed');
+    var purchase = sinon.stub(blackberry.payment, 'purchase', function(data, onsuccess, onfail) {
+      onfail();
+    });
+
+    _settings.purchaseSnoobyGold();
+    expect(snoobyPurchase.called).toBeTruthy();
+    blackberry.payment.purchase.restore();
+    document.getElementById.restore();
+  });
+
+  it('calls onSnoobyGoldPurchase() when successfully purchasing Snooby Gold', function() {
+    var getElementById = sinon.stub(document, 'getElementById');
+    getElementById.withArgs('snoobyGoldButton').returns({ setCaption: function(s) {} });
+
+    var snoobyPurchase = sinon.stub(_settings, 'onSnoobyGoldPurchase');
+    var purchase = sinon.stub(blackberry.payment, 'purchase', function(data, onsuccess, onfail) {
+      onsuccess();
+    });
+
+    _settings.purchaseSnoobyGold();
+    expect(snoobyPurchase.called).toBeTruthy();
+    blackberry.payment.purchase.restore();
+    document.getElementById.restore();
+  });
+
+  it('show support snooby form is displayed if Snooby Gold not purchased', function() {
+    var element = document; //{ getElementById: function(id) { return { style: { display: false } }; } };
+    var getElementById = sinon.stub(element, 'getElementById');
+    var supportSnoobyDisplay = sinon.spy();
+    getElementById.withArgs('supportSnooby').returns({ style: { display: false } });
+    getElementById.withArgs('snoobyGoldPurchased').returns({ style: { display: false } });
+
+    var purchase = sinon.stub(blackberry.payment, 'checkExisting', function(data, onsuccess, onfail) {
+      onsuccess({ subscriptionExists: false });
+    });
+
+    _settings.onDomReady(element);
+    expect(element.getElementById('supportSnooby').style.display).toBe('block');
+    expect(element.getElementById('snoobyGoldPurchased').style.display).toBe('none');
+    blackberry.payment.checkExisting.restore();
+    element.getElementById.restore();
+  });
+
+  it('Thank you panel is displayed if Snooby Gold is purchased', function() {
+    var element = document;//{ getElementById: function(id) { return { style: { display: false } }; } };
+    var getElementById = sinon.stub(element, 'getElementById');
+    var supportSnoobyDisplay = sinon.spy();
+    getElementById.withArgs('supportSnooby').returns({ style: { display: false } });
+    getElementById.withArgs('snoobyGoldPurchased').returns({ style: { display: false } });
+
+    var purchase = sinon.stub(blackberry.payment, 'checkExisting', function(data, onsuccess, onfail) {
+      onsuccess({ subscriptionExists: true });
+    });
+
+    _settings.onDomReady(element);
+    expect(element.getElementById('supportSnooby').style.display).toBe('none');
+    expect(element.getElementById('snoobyGoldPurchased').style.display).toBe('block');
+    blackberry.payment.checkExisting.restore();
+    element.getElementById.restore();
   });
 });
